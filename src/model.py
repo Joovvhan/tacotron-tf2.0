160985c35df19af2584649f1384b06ad0dc0ad4f
joovvhan <joovvhan.sonq@gmail.com>
2019-08-13 19:49:18 +0900

    First commit

---
+++ src/model.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+'''
+https://www.tensorflow.org/beta/tutorials/text/nmt_with_attention
+
+'''
+
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, query, values):
+        hidden_with_time_axis = tf.expand_dims(query, 1)
+
+        score = self.V(tf.nn.tanh(
+            self.W1(values) + self.W2(hidden_with_time_axis)))
+
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        context_vector = attention_weights * values
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+
+class Tacotron(tf.keras.Model):
+	def __init__(self):
+		super(Tacotron, self).__init__()
+
+		self.tokenizer 
+
+
